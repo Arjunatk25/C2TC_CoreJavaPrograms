1f22919ace4f281e9c9ce3c0872c4277f5db1009
Arjunatk25 <144626188+Arjunatk25@users.noreply.github.com>
2025-08-30 09:24:20 +0530

    commit

---
+++ C2TC_CoreJava/src/daytwo/coll.java
@@ -0,0 +1,22 @@
+package daytwo;
+import java.util.*;
+
+public class coll {
+
+	public static void main(String[] args) {
+		 ArrayList<Object> c = new ArrayList<>();
+		 
+		 c.add(10);
+		 c.add(10);
+		 c.add('a');
+		 c.add("hello");
+		 c.add("im arjun ");
+		 c.add(true);
+		 c.add(100.00);
+		 c.add(77777777);
+		 
+		 
+		 System.out.println(c.toString());
+
+	}	 
+}
